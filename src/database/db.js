ea9f14735fb535bd4f78915999b852db52aac5bf
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2020-08-07 13:58:53 -0300

    Criando o banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,33 @@
+const Database = require('sqlite-async')
+Database.open(__dirname + '/database.sqlite').then(execute)
+
+function execute(db) {
+   //Criar as tabelas do banco de dados.
+    db.exec(`
+    CREATE TABLE IF NOT EXISTS proffys (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        whatsapp TEXT,
+        bio TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS classes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        subject TEXT,
+        cost TEXT,
+        proffy_id INTEGER
+    );
+
+    CREATE TABLE IF NOT EXISTS class_schedule (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        class_id INTEGER,
+        weekday INTEGER,
+        time_from INTEGER,
+        time_to INTEGER
+
+    );
+   
+   `)
+
+}
